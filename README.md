61b9e3ccad0ef924c1c47bdd0dca446c9dd063af
Zack Slayton <zslayton@amazon.com>
2020-07-02 14:30:19 -1000

    Added `dump` command.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+This package is considered experimental. It is under active/early development,
+and the API is subject to change.
+
+## Developer notes
+
 Run the following command to initialize all of the necessary git submodules.
 ```
 git submodule update --init --recursive
--- build.rs
+++ build.rs
@@ -3,10 +3,8 @@ use std::fs::create_dir_all;
 use std::path::Path;
 use std::process;
 
-const BUILD_DIR: &str = "./ion-c/build/release";
-
 fn main() {
-    let build_dir = Path::new(BUILD_DIR);    
+    let build_dir = Path::new("./ion-c/build/release");
 
     // Create the ion-c build directory if necessary
     if !build_dir.is_dir() {
@@ -25,6 +23,29 @@ fn main() {
 
     // Output lines that start with "cargo:" are interpreted by Cargo. See the docs for details:
     // https://doc.rust-lang.org/cargo/reference/build-scripts.html#outputs-of-the-build-script
+
+    // The `ion` executable statically links to the `ion-c` CLI. The following output tells Cargo
+    // which libraries to link against and in which directories they can be found.
+
+    // ion_events library
+    println!("cargo:rustc-link-search=native=./ion-c/build/release/build/tools/events");
+    println!("cargo:rustc-link-lib=static=ion_events_static");
+
+    // ion_c library
+    println!("cargo:rustc-link-search=native=./ion-c/build/release/build/ionc");
+    println!("cargo:rustc-link-lib=static=ionc_static");
+
+    // decNumber library
+    println!("cargo:rustc-link-search=native=./ion-c/build/release/build/decNumber");
+    println!("cargo:rustc-link-lib=static=decNumberStatic");
+
+    // C++ library
+    println!("cargo:rustc-link-search=native=/usr/lib");
+    println!("cargo:rustc-link-lib=c++");
+
+    // ion-c CLI library
+    println!("cargo:rustc-link-search=native=./ion-c/build/release/build/tools/cli/");
+    println!("cargo:rustc-link-lib=static=ion_cli_main");
 
     // Only rebuild ion-c if that submodule directory is updated
     println!("cargo:rereun-if-changed={}", build_dir.display());
