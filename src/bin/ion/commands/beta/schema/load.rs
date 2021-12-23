5ef8953fdb570d7aae0e238492b4b64822c36632
Khushboo <68757952+desaikd@users.noreply.github.com>
2021-12-23 10:28:57 -0800

    adds changes for schema subcommand to beta (#16)

---
+++ src/bin/ion/commands/beta/schema/load.rs
@@ -0,0 +1,64 @@
+use anyhow::{Result};
+use clap::{App, Arg, ArgMatches};
+use ion_schema::authority::{DocumentAuthority, FileSystemDocumentAuthority};
+use std::path::Path;
+use ion_schema::system::SchemaSystem;
+
+const ABOUT: &str = "Loads an Ion Schema file using user provided schema id and returns a result message. Shows an error message if there were any invalid schema syntax found during the load process";
+
+// Creates a `clap` (Command Line Arguments Parser) configuration for the `load` command.
+// This function is invoked by the `load` command's parent `schema`, so it can describe its
+// child commands.
+pub fn app() -> App<'static, 'static> {
+    App::new("load")
+        .about(ABOUT)
+        .arg(
+            // Input file can be specified by the "-s" or "--schema" flags.
+            Arg::with_name("schema")
+                .long("schema")
+                .short("s")
+                .required(true)
+                .takes_value(true)
+                .value_name("SCHEMA")
+                .help("The Ion Schema file to load"),
+        )
+        .arg(
+            // Directory(s) that will be used as authority(s) for schema system
+            Arg::with_name("directories")
+                .long("directory")
+                .short("d")
+                .min_values(1)
+                .takes_value(true)
+                .multiple(true)
+                .value_name("DIRECTORY")
+                .required(true)
+                .help("One or more directories that will be searched for the requested schema"),
+        )
+}
+
+// This function is invoked by the `load` command's parent `schema`.
+pub fn run(_command_name: &str, matches: &ArgMatches<'static>) -> Result<()> {
+    // Extract the user provided document authorities/ directories
+    let authorities: Vec<_> = matches.values_of("directories").unwrap().collect();
+
+    // Extract schema file provided by user
+    let schema_id = matches.value_of("schema").unwrap();
+
+    // Set up document authorities vector
+    let mut document_authorities: Vec<Box<dyn DocumentAuthority>> = vec![];
+
+    for authority in authorities {
+        document_authorities.push(Box::new(FileSystemDocumentAuthority::new(Path::new(
+            authority,
+        ))))
+    }
+
+    // Create a new schema system from given document authorities
+    let mut schema_system = SchemaSystem::new(document_authorities);
+
+    // load given schema
+    println!("Schema: {:#?}", schema_system.load_schema(schema_id)?);
+
+    Ok(())
+}
+
---
+++ src/bin/ion/commands/beta/schema/mod.rs
@@ -0,0 +1,49 @@
+pub mod load;
+
+use anyhow::Result;
+use clap::{App, ArgMatches};
+use crate::commands::{CommandRunner, CommandConfig};
+
+// To add a schema subcommand, add your new command to the `schema_subcommands`
+// and `runner_for_schema_subcommands` functions.
+
+// Creates a Vec of CLI configurations for all of the available built-in subcommands for schema
+pub fn schema_subcommands() -> Vec<CommandConfig> {
+    vec![
+        load::app(),
+    ]
+}
+
+pub fn runner_for_schema_subcommand(command_name: &str) -> Option<CommandRunner> {
+    let runner = match command_name {
+        "load" => load::run,
+        _ => return None
+    };
+    Some(runner)
+}
+
+// The functions below are used by the `beta` subcommand when `schema` is invoked.
+pub fn run(_command_name: &str, matches: &ArgMatches<'static>) -> Result<()> {
+    // We want to evaluate the name of the subcommand that was invoked
+    let (command_name, command_args) = matches.subcommand();
+    if let Some(runner) = runner_for_schema_subcommand(command_name) {
+        // If a runner is registered for the given command name, command_args is guaranteed to
+        // be defined; we can safely unwrap it.
+        runner(command_name, command_args.unwrap())?;
+    } else {
+        let message = format!(
+            "The requested schema command ('{}') is not supported and clap did not generate an error message.",
+            command_name
+        );
+        unreachable!(message);
+    }
+    Ok(())
+}
+
+pub fn app() -> CommandConfig {
+    App::new("schema")
+        .about(
+            "The 'schema' command is a namespace for commands that are related to schema sandbox",
+        )
+        .subcommands(schema_subcommands())
+}
