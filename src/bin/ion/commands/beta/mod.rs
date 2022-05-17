7ca2380d143ff4b423f4a69ce5cc89fa3e75e890
Zack Slayton <zslayton@amazon.com>
2022-05-17 15:40:10 -0400

    Updating ion-rs to v0.10.0

--- src/bin/ion/commands/beta/mod.rs
+++ src/bin/ion/commands/beta/mod.rs
@@ -1,26 +1,23 @@
 pub mod inspect;
 pub mod schema;
 
+use crate::commands::{CommandConfig, CommandRunner};
 use anyhow::Result;
 use clap::{App, ArgMatches};
-use crate::commands::{CommandRunner, CommandConfig};
 
 // To add a beta subcommand, add your new command to the `beta_subcommands`
 // and `runner_for_beta_subcommands` functions.
 
 // Creates a Vec of CLI configurations for all of the available built-in commands
 pub fn beta_subcommands() -> Vec<CommandConfig> {
-    vec![
-        inspect::app(),
-        schema::app()
-    ]
+    vec![inspect::app(), schema::app()]
 }
 
 pub fn runner_for_beta_subcommand(command_name: &str) -> Option<CommandRunner> {
     let runner = match command_name {
         "inspect" => inspect::run,
         "schema" => schema::run,
-        _ => return None
+        _ => return None,
     };
     Some(runner)
 }
