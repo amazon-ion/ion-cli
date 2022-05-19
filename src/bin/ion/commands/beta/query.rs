8ea0b309b0943c059c5b9ca421938a1f27b66ef4
Joshua Barr <jobarr@amazon.com>
2022-05-18 17:43:22 -0700

    Expand query parsing a bit

--- src/bin/ion/commands/beta/query.rs
+++ src/bin/ion/commands/beta/query.rs
@@ -1,17 +1,17 @@
 use clap::{App, Arg, ArgMatches};
 use anyhow::{Result, Context};
 use std::{fs, io};
-use nom::IResult;
-use nom::combinator::{recognize, not};
-use nom::sequence::{pair, terminated};
-use nom::branch::alt;
-use nom::multi::many0_count;
-use nom::character::complete::{one_of, satisfy};
-use std::io::Read;
-use ion_rs::value::reader::{element_reader, ElementReader};
-use ion_rs::value::owned::{OwnedElement};
+use nom::*;
+use std::io::{Read};
+use ion_rs::value::reader::{ElementReader, element_reader};
+use ion_rs::value::owned::{OwnedElement, OwnedStruct};
 use ion_rs::value::{Element, Struct};
-use ion_rs::value::writer::{ElementWriter, Format, TextKind};
+use ion_rs::value::writer::{ElementWriter, Format, TextKind, SliceElementWriter};
+use nom::combinator::*;
+use nom::sequence::*;
+use nom::branch::*;
+use nom::multi::*;
+use nom::character::complete::*;
 
 const ABOUT: &str =
     "A command-line processor for Ion.";
@@ -27,6 +27,13 @@ pub fn app() -> App<'static, 'static> {
                 .long("jq")
                 .short("j")
                 .help("Uses jq query syntax."),
+        )
+        //TODO: Implement -r
+        .arg(
+            Arg::with_name("raw")
+                .long("raw")
+                .short("r")
+                .help("Print bare strings (useful for consuming with other tools)")
         )
         .arg(
             Arg::with_name("QUERY")
@@ -56,40 +63,74 @@ pub fn run(_command_name: &str, matches: &ArgMatches<'static>) -> Result<()> {
     };
 
     // get the identifier from given query
-    let identifier = identifier(query).unwrap();
-    // println!("{}", identifier.1);
+    let (_remaining, jqExpression) = expression(query).unwrap();
 
-    // send input to stdout
-    // io::copy(&mut rdr, &mut io::stdout()).unwrap();
+    println!("jqExpression: {:?}", jqExpression);
 
+    println!("Reading in Ion data...");
     // read the given input Ion file with element reader
+    //TODO: Stream, don't load whole file to memory
     let mut ion_buffer = Vec::new();
     rdr.read_to_end(&mut ion_buffer)?;
 
-    let owned_elements: Vec<OwnedElement> = element_reader()
-        .read_all(&ion_buffer)
+    //TODO: Use native reader and writer
+    let mut ion_iter = element_reader()
+        .iterate_over(&ion_buffer)
         .with_context(|| "Could not parse Ion file")?;
 
-    // filter owned elements that match with given query
-    let mut result: Vec<&OwnedElement> = vec![];
-    for owned_element in owned_elements.iter() {
-        match owned_element.as_struct() {
-            None => {
-            }
-            Some(ion_struct) => {
-                result.extend(ion_struct.get_all(identifier.1));
-            }
+    let mut foobar = Box::new(ion_iter.map(|oe| &oe.unwrap()));
+
+    println!("Output: ");
+
+
+    match jqExpression {
+        JqExpression::Dot => {
+            // identity, do nothing
+        }
+        JqExpression::Field(name) => {
+            foobar = Box::new(foobar.flat_map(|oe| select_field(name, oe)));
         }
     }
 
-    // write out the filtered result
-    let mut element_writer = Format::Text(TextKind::Pretty).element_writer_for_slice(&mut ion_buffer)?;
-    result.iter().for_each(|v| element_writer.write(*v).unwrap());
-    let slice = element_writer.finish()?;
-    let output = String::from_utf8_lossy(slice);
+    ion_iter.map(|oe| print(oe));
 
-    println!("Output:\n{}", output);
     Ok(())
+}
+
+//TODO: this should be a function that we can use with flat_map on an iter
+fn select_field<'a>(field_name: String, owned_element: &'a OwnedElement) -> Box<dyn Iterator<Item=&OwnedElement> + 'a> {
+    if let Some(ion_struct) = owned_element.as_struct() {
+        return ion_struct.get_all(field_name);
+    };
+    Box::new(std::iter::empty())
+}
+
+fn print(ion_element: &OwnedElement) -> Result<()> {
+    //TODO: Handle arbitrarily-sized output objects, or at least larger ones
+    let mut buf = vec![0u8; 4096];
+    let mut writer = Format::Text(TextKind::Compact).element_writer_for_slice(&mut buf)?;
+    writer.write(ion_element)?;
+    let result = writer.finish()?;
+
+    println!("{}", String::from_utf8_lossy(result).to_string());
+    Ok(())
+}
+
+
+// Recognize something like `.foo`
+// Yields Ok("", "foo")
+// .foo.bar
+// .foo | .bar
+fn field(input: &str) -> IResult<&str, JqExpression> {
+    map(preceded(dot, identifier),
+        |name| JqExpression::Field(name.to_string()))(input)
+}
+
+fn expression(input: &str) -> IResult<&str, JqExpression> {
+    alt((
+        dot,
+        field
+    ))(input)
 }
 
 fn identifier(input: &str) -> IResult<&str, &str> {
@@ -114,4 +155,15 @@ fn identifier_trailing_character(input: &str) -> IResult<&str, char> {
 /// Matches characters that are legal in an identifier, though not necessarily at the beginning.
 fn identifier_trailing_characters(input: &str) -> IResult<&str, &str> {
     recognize(many0_count(identifier_trailing_character))(input)
+}
+
+fn dot(input:&str) -> IResult<&str, JqExpression> {
+    map(char('.'), |_| JqExpression::Dot)(input)
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) enum JqExpression {
+    Dot,
+    Field(String),
 }
