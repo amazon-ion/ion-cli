8913607c59164ac1ba47e59ba9156740e3eba4a3
Khushboo Desai <desaikd@amazon.com>
2024-04-05 14:12:08 -0700

    Adds changes to sort schema file paths to preserve anonymous type names

--- code-gen-projects/java/code-gen-demo/src/test/java/org/example/CodeGenTest.java
+++ code-gen-projects/java/code-gen-demo/src/test/java/org/example/CodeGenTest.java
@@ -11,14 +11,14 @@ class CodeGenTest {
          a.add("foo");
          a.add("bar");
          a.add("baz");
-         StructWithFields s = new StructWithFields("hello", 12, new AnonymousType1(a));
+         StructWithFields s = new StructWithFields("hello", 12, new AnonymousType2(a));
          assertEquals("hello", s.getA(), "s.getA() should return \"hello\"");
          assertEquals(12, s.getB(), "s.getB() should return `12`");
          assertEquals(3, s.getC().getValue().size(), "s.getC().getValue() should return ArrayList fo size 3");
     }
 
     @Test void getterAndSetterTestForNestedStruct() {
-             NestedStruct n = new NestedStruct("hello", 12, new AnonymousType2(false));
+             NestedStruct n = new NestedStruct("hello", 12, new AnonymousType1(false));
              assertEquals("hello", n.getA(), "n.getA() should return \"hello\"");
              assertEquals(12, n.getB(), "n.getB() should return `12`");
              assertEquals(false, n.getC().getD(), "n.getC().getD() should return `false`");
