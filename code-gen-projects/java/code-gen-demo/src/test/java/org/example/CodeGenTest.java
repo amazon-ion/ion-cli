f3353491b6f3e585eddbda310563541242bac969
Khushboo Desai <desaikd@amazon.com>
2024-04-15 12:04:12 -0700

    Adds write API generation support
    * Modifies class template toa dd writer API support
    * Adds roundtrip tests in CodeGenTest.java

--- code-gen-projects/java/code-gen-demo/src/test/java/org/example/CodeGenTest.java
+++ code-gen-projects/java/code-gen-demo/src/test/java/org/example/CodeGenTest.java
@@ -5,13 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import com.amazon.ion.system.IonReaderBuilder;
 import com.amazon.ion.IonReader;
+import com.amazon.ion.system.IonTextWriterBuilder;
+import com.amazon.ion.IonWriter;
+import com.amazon.ion.IonSystem;
+import com.amazon.ion.system.IonSystemBuilder;
+import com.amazon.ion.IonLoader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileInputStream;
 import java.io.File;
 
 class CodeGenTest {
-    // TODO: Add roundtrip tests after read-write APIs are supported for Java code generation
+    private static final IonSystem ionSystem = IonSystemBuilder.standard().build();
+    private static final IonLoader ionLoader = ionSystem.getLoader();
+
     @Test void getterAndSetterTestForStructWithFields() {
          ArrayList<String> a = new ArrayList<String>();
          a.add("foo");
@@ -31,32 +39,35 @@ class CodeGenTest {
          assertEquals(false, n.getC().getD(), "n.getC().getD() should return `false`");
     }
 
-    @Test void readFromForStructWithFields() throws IOException {
+    @Test void roundtripTestForStructWithFields() throws IOException {
         File f = new File("./../../input/struct_with_fields.ion");
         InputStream inputStream = new FileInputStream(f);
-
+        IonTextWriterBuilder b = IonTextWriterBuilder.standard();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
         IonReaderBuilder readerBuilder = IonReaderBuilder.standard();
         try (IonReader reader = readerBuilder.build(inputStream)) {
             reader.next();
             StructWithFields s = StructWithFields.readFrom(reader);
-            assertEquals("hello", s.getA(), "s.getA() should return \"hello\"");
-            assertEquals(12, s.getB(), "s.getB() should return `12`");
-            assertEquals(3, s.getC().getValue().size(), "s.getC().getValue() should return ArrayList fo size 3");
-            assertEquals(10e2, s.getD(), "s.getD() should return `10e2`");
+            IonWriter writer = b.build(out);
+            s.writeTo(writer);
+            writer.close();
         }
+        assertEquals(ionLoader.load(f), ionLoader.load(out.toByteArray()));
     }
 
-    @Test void readFromForNestedStruct() throws IOException {
+    @Test void roundtripTestForNestedStruct() throws IOException {
         File f = new File("./../../input/nested_struct.ion");
         InputStream inputStream = new FileInputStream(f);
-
+        IonTextWriterBuilder b = IonTextWriterBuilder.standard();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
         IonReaderBuilder readerBuilder = IonReaderBuilder.standard();
         try (IonReader reader = readerBuilder.build(inputStream)) {
             reader.next();
             NestedStruct n = NestedStruct.readFrom(reader);
-            assertEquals("hello", n.getA(), "n.getA() should return \"hello\"");
-            assertEquals(12, n.getB(), "n.getB() should return `12`");
-            assertEquals(false, n.getC().getD(), "n.getC().getD() should return `false`");
+            IonWriter writer = b.build(out);
+            n.writeTo(writer);
+            writer.close();
         }
+        assertEquals(ionLoader.load(f), ionLoader.load(out.toByteArray()));
     }
 }
