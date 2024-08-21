eefcbf0b1abb42c4835d63c768ce6ea40007d0fa
Khushboo Desai <desaikd@amazon.com>
2024-08-21 16:24:58 -0700

    Adds `From` impls for builder errors to be converetd to `CodeGenError`

--- src/bin/ion/commands/generate/result.rs
+++ src/bin/ion/commands/generate/result.rs
@@ -1,4 +1,8 @@
+use crate::commands::generate::model::{
+    ScalarBuilderError, SequenceBuilderError, StructureBuilderError, WrappedScalarBuilderError,
+};
 use ion_schema::result::IonSchemaError;
+use std::error::Error;
 use thiserror::Error;
 
 /// Represents code generation result
@@ -24,6 +28,8 @@ pub enum CodeGenError {
     },
     #[error("{description}")]
     InvalidDataModel { description: String },
+    #[error("{description}")]
+    DataModelBuilderError { description: String },
 }
 
 /// A convenience method for creating an CodeGen containing an CodeGenError::InvalidDataModel
@@ -32,4 +38,44 @@ pub fn invalid_abstract_data_type_error<T, S: AsRef<str>>(description: S) -> Cod
     Err(CodeGenError::InvalidDataModel {
         description: description.as_ref().to_string(),
     })
+}
+
+/// A convenience method for creating an CodeGenError::InvalidDataModel
+/// with the provided description text.
+pub fn invalid_abstract_data_type_raw_error<S: AsRef<str>>(description: S) -> CodeGenError {
+    CodeGenError::InvalidDataModel {
+        description: description.as_ref().to_string(),
+    }
+}
+
+impl From<WrappedScalarBuilderError> for CodeGenError {
+    fn from(value: WrappedScalarBuilderError) -> Self {
+        CodeGenError::DataModelBuilderError {
+            description: value.to_string(),
+        }
+    }
+}
+
+impl From<ScalarBuilderError> for CodeGenError {
+    fn from(value: ScalarBuilderError) -> Self {
+        CodeGenError::DataModelBuilderError {
+            description: value.to_string(),
+        }
+    }
+}
+
+impl From<SequenceBuilderError> for CodeGenError {
+    fn from(value: SequenceBuilderError) -> Self {
+        CodeGenError::DataModelBuilderError {
+            description: value.to_string(),
+        }
+    }
+}
+
+impl From<StructureBuilderError> for CodeGenError {
+    fn from(value: StructureBuilderError) -> Self {
+        CodeGenError::DataModelBuilderError {
+            description: value.to_string(),
+        }
+    }
 }
