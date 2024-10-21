6b4e9db34c432f316126cc7fb76f741561798717
Matthew Pope <81593196+popematt@users.noreply.github.com>
2024-10-21 12:34:26 -0700

    Direct users to provide feedback on GitHub (#156)

--- src/bin/ion/main.rs
+++ src/bin/ion/main.rs
@@ -5,12 +5,8 @@ mod input;
 mod output;
 mod transcribe;
 
-use anyhow::Result;
-use commands::{IonCliCommand, IonCliNamespace};
-use ion_rs::IonError;
-use std::io::ErrorKind;
-
 use crate::commands::cat::CatCommand;
+use crate::commands::complaint::SucksCommand;
 use crate::commands::from::FromNamespace;
 use crate::commands::generate::GenerateCommand;
 use crate::commands::hash::HashCommand;
@@ -21,10 +17,16 @@ use crate::commands::schema::SchemaNamespace;
 use crate::commands::stats::StatsCommand;
 use crate::commands::symtab::SymtabNamespace;
 use crate::commands::to::ToNamespace;
+use anyhow::Result;
+use commands::{IonCliCommand, IonCliNamespace};
+use ion_rs::IonError;
+use std::io::ErrorKind;
 
 fn main() -> Result<()> {
     let root_command = RootCommand;
-    let args = root_command.clap_command().get_matches();
+    let args = root_command.clap_command()
+        .after_help("Having a problem with Ion CLI?\nOpen an issue at https://github.com/amazon-ion/ion-cli/issues/new/choose")
+        .get_matches();
     let mut command_path: Vec<String> = vec![IonCliNamespace::name(&root_command).to_owned()];
 
     if let Err(e) = root_command.run(&mut command_path, &args) {
@@ -65,6 +67,7 @@ impl IonCliNamespace for RootCommand {
             Box::new(SymtabNamespace),
             Box::new(ToNamespace),
             Box::new(StatsCommand),
+            Box::new(SucksCommand),
         ]
     }
 }
